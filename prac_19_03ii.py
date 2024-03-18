e1ce1be0d67858eeb5f10b285aa215580730227d
MohammadRakiburRahman <mohammad.rahman2@northsouth.edu>
2024-03-19 01:53:00 +0600

    instance practice

---
+++ prac_19_03ii.py
@@ -0,0 +1,18 @@
+###instances  practice
+
+
+class MyFirstClass:
+    def __init__(self) :
+        print ("who wrote this?")
+        return print
+
+#index = "Author-Book"
+
+class MyFirstClass:
+    def hand_list(self,philosopher,book):
+        #print("class variable",index)
+        print(f"{philosopher} wrote the book : {book}")
+
+whodunnit = MyFirstClass()
+whodunnit.hand_list("rakib","do code")
+        
