d63817cc8b57be537267f34b8b12985d6295ddd1
MohammadRakiburRahman <mohammad.rahman2@northsouth.edu>
2024-03-19 01:55:17 +0600

    corrected code

--- prac_19_03ii.py
+++ prac_19_03ii.py
@@ -1,18 +1,11 @@
-###instances  practice
+# Sample Solution code
+class MyFirstClass():
+    print("Who wrote this?")
+    index = "Author-Book"
 
-
-class MyFirstClass:
-    def __init__(self) :
-        print ("who wrote this?")
-        return print
-
-#index = "Author-Book"
-
-class MyFirstClass:
-    def hand_list(self,philosopher,book):
-        #print("class variable",index)
-        print(f"{philosopher} wrote the book : {book}")
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
 
 whodunnit = MyFirstClass()
-whodunnit.hand_list("rakib","do code")
-        
+whodunnit.hand_list("Sun Tzu", "The Art of War")
